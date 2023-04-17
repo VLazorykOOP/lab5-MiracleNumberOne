e9b962b760c3c526696edd1663e7c7f37ef7c34c
Miracle179 <prsky.ivan@gmail.com>
2023-04-17 18:42:14 +0200

    SportGame - клас, який представляє загальну спортивну гру з типом гри та кількістю гравців як її атрибути. Він також має функції для операцій введення та виведення.
    Футбол — це похідний клас, який успадковує базовий клас SportGame. Він представляє футбольну гру та розширює функціональні можливості класу SportGame, додаючи два додаткові атрибути: team1 і team2, які представляють назви двох футбольних команд, які грають у гру. Клас Football також замінює функції введення та виведення класу SportGame, щоб забезпечити спеціалізовані операції введення/виведення для футбольної гри.
    
    Обидва класи SportGame і Football визначають конструктори для конструкції за замовчуванням, параметризованої конструкції, конструкції копії та конструкції руху. Вони також визначають оператори присвоєння для присвоєння та переміщення. Клас Football замінює оператори призначення класу SportGame, щоб переконатися, що додаткові атрибути team1 і team2 правильно призначені під час призначення об’єкта або призначення ходу.

--- OOPLab5T/OOPLab5T.cpp
+++ OOPLab5T/OOPLab5T.cpp
@@ -115,6 +115,135 @@ public:
     }
 };
 
+class SportGame {
+protected:
+    std::string gameType;
+    int numOfPlayers;
+public:
+    // Конструктор за замовчуванням
+    SportGame() : gameType(""), numOfPlayers(0) {}
+    
+    // Конструктор з параметрами
+    SportGame(const std::string& type, int numPlayers) : gameType(type), numOfPlayers(numPlayers) {}
+    
+    // Конструктор копіювання
+    SportGame(const SportGame& other) : gameType(other.gameType), numOfPlayers(other.numOfPlayers) {}
+    
+    // Конструктор перенесення
+    SportGame(SportGame&& other) : gameType(std::move(other.gameType)), numOfPlayers(other.numOfPlayers) {}
+    
+    // Оператор присвоєння
+    SportGame& operator=(const SportGame& other) {
+        if (this == &other) return *this; // Перевірка на самоприсвоєння
+        
+        gameType = other.gameType;
+        numOfPlayers = other.numOfPlayers;
+        
+        return *this;
+    }
+    
+    // Оператор перенесення
+    SportGame& operator=(SportGame&& other) {
+        if (this == &other) return *this; // Перевірка на самоприсвоєння
+        
+        gameType = std::move(other.gameType);
+        numOfPlayers = other.numOfPlayers;
+        
+        return *this;
+    }
+    
+    // Функції вводу/виводу
+    virtual void input(std::istream& is) {
+        std::cout << "Введіть тип гри: ";
+        is >> gameType;
+        std::cout << "Введіть кількість гравців: ";
+        is >> numOfPlayers;
+    }
+    
+    virtual void output(std::ostream& os) const {
+        os << "Тип гри: " << gameType << ", Кількість гравців: " << numOfPlayers;
+    }
+    
+    friend std::istream& operator>>(std::istream& is, SportGame& game) {
+        game.input(is);
+        return is;
+    }
+    
+    friend std::ostream& operator<<(std::ostream& os, const SportGame& game) {
+        game.output(os);
+        return os;
+    }
+};
+
+class Football : public SportGame {
+private:
+    std::string team1;
+    std::string team2;
+public:
+    // Конструктор за замовчуванням
+    Football() : SportGame(), team1(""), team2("") {}
+    
+    // Конструктор з параметрами
+    Football(const std::string& type, int numPlayers, const std::string& t1, const std::string& t2)
+        : SportGame(type, numPlayers), team1(t1), team2(t2) {}
+    
+    // Конструктор копіювання
+    Football(const Football& other)
+        : SportGame(other), team1(other.team1), team2(other.team2) {}
+    
+    // Конструктор перенесення
+    Football(Football&& other)
+        : SportGame(std::move(other)), team1(std::move(other.team1)), team2(std::move(other.team2)) {}
+    
+    // Оператор присвоєння
+    Football& operator=(const Football& other) {
+        if (this == &other) return *this; // Перевірка на самоприсвоєння
+        
+        SportGame::operator=(other); // Виклик оператора присвоєння базового класу
+        
+        team1 = other.team1;
+        team2 = other.team2;
+        
+        return *this;
+    }
+    
+    // Оператор перенесення
+    Football& operator=(Football&& other) {
+        if (this == &other) return *this; // Перевірка на самоприсвоєння
+        
+        SportGame::operator=(std::move(other)); // Виклик оператора перенесення базового класу
+        
+        team1 = std::move(other.team1);
+        team2 = std::move(other.team2);
+        
+        return *this;
+    }
+    
+    // Функції вводу/виводу
+    void input(std::istream& is) override {
+        SportGame::input(is); // Виклик функції вводу базового класу
+        std::cout << "Введіть назву команди 1: ";
+        is >> team1;
+        std::cout << "Введіть назву команди 2: ";
+        is >> team2;
+    }
+    
+    void output(std::ostream& os) const override {
+        SportGame::output(os); // Виклик функції виводу базового класу
+        os << ", Команда 1: " << team1 << ", Команда 2: " << team2;
+    }
+    
+    friend std::istream& operator>>(std::istream& is, Football& football) {
+        football.input(is);
+        return is;
+    }
+    
+    friend std::ostream& operator<<(std::ostream& os, const Football& football) {
+        football.output(os);
+        return os;
+    }
+};
+
 
 int main() {
     // Тестування класу Студент
@@ -137,5 +266,39 @@ int main() {
     // Виклик функції друку
     apartment.print();
 
+
+    // Створення об'єкту класу Football за допомогою конструктора за замовчуванням
+    Football football1;
+    std::cout << "Футбол 1: " << football1 << std::endl;
+    
+    // Введення даних в об'єкт football1
+    std::cin >> football1;
+    
+    // Створення об'єкту класу Football за допомогою конструктора з параметрами
+    Football football2("Футбол", 11, "Команда 1", "Команда 2");
+    std::cout << "Футбол 2: " << football2 << std::endl;
+    
+    // Введення даних в об'єкт football2
+    std::cin >> football2;
+    
+    // Створення об'єкту класу Football за допомогою конструктора копіювання
+    Football football3 = football2;
+    std::cout << "Футбол 3: " << football3 << std::endl;
+    
+    // Створення об'єкту класу Football за допомогою конструктора перенесення
+    Football football4 = std::move(football3);
+    std::cout << "Футбол 4: " << football4 << std::endl;
+    
+    // Введення даних в об'єкт football4
+    std::cin >> football4;
+    
+    // Оператор присвоєння
+    football1 = football4;
+    std::cout << "Футбол 1 (після оператора присвоєння): " << football1 << std::endl;
+    
+    // Оператор перенесення
+    football2 = std::move(football4);
+    std::cout << "Футбол 2 (після оператора перенесення): " << football2 << std::endl;
+    
     return 0;
 }
