f905e2b86184fd2ec5f06192124c33c4e50a61a5
Miracle179 <prsky.ivan@gmail.com>
2023-04-17 18:13:53 +0200

    Код вище реалізує два класи: "Студент" та "Студент-дипломник".
    Клас "Студент" має поля: ПІБ (прізвище, ім'я, по-батькові), курс та ідентифікаційний номер. Він має конструктор з параметрами, деструктор та функцію друку.
    
    Клас "Студент-дипломник" є похідним класом від класу "Студент" і має додаткове поле - тему диплома. Він має конструктор за замовчуванням та конструктор з параметрами, деструктор та функцію друку. Клас також має функції перепризначення назви диплома та ідентифікаційного номера.
    
    У функції main() проводиться тестування функціоналу обох класів. Створюються об'єкти класів "Студент" та "Студент-дипломник", викликаються функції друку для виведення інформації про цих студентів, а також проводиться перепризначення назви диплома та ідентифікаційного номера для об'єкту класу "Студент-дипломник".

--- OOPLab5T/OOPLab5T.cpp
+++ OOPLab5T/OOPLab5T.cpp
@@ -1,24 +1,74 @@
-﻿// OOPLab5T.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <string>
 
+class Student {
+protected:
+    std::string name;
+    int course;
+    int id;
 
-#include <iostream>
+public:
+    // Конструктор з параметрами
+    Student(const std::string& name, int course, int id)
+        : name(name), course(course), id(id) {}
 
-// Ваші файли загловки 
-//
-#include "Lab5Exmaple.h"
-int main()
-{
-    std::cout << " Lab #5  !\n";
-    //  Код виконання завдань
-    //  Головне меню завдань
-    //  Функції та класи можуть знаходитись в інших файлах проекту
+    // Деструктор
+    ~Student() {
+        std::cout << "Деструктор класу Студент: " << name << std::endl;
+    }
 
-    int chain = 1;
-    if (chain == 1) chain = mainExample1();
-    if (chain == 2) chain = mainExample2();
-    if (chain == 3) chain = mainExample3();
-    if (chain == 4) chain = mainExample4();
-    if (chain == 5) chain = mainExample5();
+    // Функція друку
+    void print() const {
+        std::cout << "ПІБ: " << name << ", Курс: " << course << ", ID: " << id << std::endl;
+    }
+};
 
+class StudentDiploma : public Student {
+private:
+    std::string thesisTopic;
+
+public:
+    // Конструктор за замовчуванням
+    StudentDiploma() : thesisTopic("") {}
+
+    // Конструктор з параметрами
+    StudentDiploma(const std::string& name, int course, int id, const std::string& thesisTopic)
+        : Student(name, course, id), thesisTopic(thesisTopic) {}
+
+    // Деструктор
+    ~StudentDiploma() {
+        std::cout << "Деструктор класу Студент-дипломник: " << name << std::endl;
+    }
+
+    // Функція друку
+    void print() const {
+        std::cout << "ПІБ: " << name << ", Курс: " << course << ", ID: " << id
+                  << ", Тема диплома: " << thesisTopic << std::endl;
+    }
+
+    // Функції перепризначення назви диплома та ідентифікаційного номера
+    void setThesisTopic(const std::string& thesisTopic) {
+        this->thesisTopic = thesisTopic;
+    }
+
+    void setId(int id) {
+        this->id = id;
+    }
+};
+
+int main() {
+    // Тестування класу Студент
+    Student student1("Іванов Іван Іванович", 2, 12345);
+    student1.print();
+
+    // Тестування класу Студент-дипломник
+    StudentDiploma student2("Петров Петро Петрович", 4, 56789, "Аналіз даних");
+    student2.print();
+
+    // Перепризначення назви диплома та ідентифікаційного номера
+    student2.setThesisTopic("Машинне навчання");
+    student2.setId(98765);
+    student2.print();
+
+    return 0;
 }
